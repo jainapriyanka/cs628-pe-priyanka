644ef1c220ba5ce9e1ae89434f7695c9242c47f6
jainapriyanka <147120582+jainapriyanka@users.noreply.github.com>
2025-02-03 16:35:13 -0800

    Update App.js

--- PE03-ToDo-List-App/todo/src/App.js
+++ PE03-ToDo-List-App/todo/src/App.js
@@ -2,23 +2,31 @@ import React, { useState } from "react";
 import "./index.css";
 
 function ToDoListApp() {
+  // This state used to store the list of tasks
   const [tasks, setTasks] = useState([]);
+  // This state used to store the text input for a new task
   const [taskInput, setTaskInput] = useState("");
 
+  // This addTask function used to add a new task to the list
   const addTask = () => {
+    // It checks if the input is not empty
     if (taskInput.trim() !== "") {
       setTasks([...tasks, taskInput]);
       setTaskInput("");
     }
   };
 
+  // This deleteTask function used to remove a task from the list
   const deleteTask = (index) => {
     setTasks(tasks.filter((_, i) => i !== index));
   };
 
   return (
     <div className="container">
+      {/* Title */}
       <h2>ToDo List App</h2>
+
+      {/* Input box and Add Task button */}
       <div className="input-container">
         <input
           type="text"
@@ -32,6 +40,7 @@ function ToDoListApp() {
         {tasks.map((task, index) => (
           <li key={index} className="task-item">
             <span>{task}</span>
+            {/* Adding delete button for each task */}
             <button className="delete-btn" onClick={() => deleteTask(index)}>Delete</button>
           </li>
         ))}
